1150742d09d247439478598c85b58b9e22099fe2
PAVAN KUMAR GOGULA <143589457+GogulaPavan@users.noreply.github.com>
2025-02-23 14:15:11 +0530

    Add files via upload

---
+++ frontend/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: "standalone", // Ensures Vercel builds properly
+};
+
+module.exports = nextConfig;
