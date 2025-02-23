61bb1eab361062e3f966a05bd3dfa481c8b9c3fb
PAVAN KUMAR GOGULA <143589457+GogulaPavan@users.noreply.github.com>
2025-02-23 14:22:17 +0530

    Delete frontend directory

--- frontend/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: "standalone", // Ensures Vercel builds properly
-};
-
-module.exports = nextConfig;
